f5b2cdab78a7213577760e2d060ea41fdc26472c
Bryan Samuels <bryansamuels123@gmail.com>
2023-09-12 21:30:31 -0500

    final result 'pre-test' save

--- Data-Functions/loadData.py
+++ Data-Functions/loadData.py
@@ -151,10 +151,142 @@ def loadPlayerData(test):
         print("Error Loading the playerData")
         return -1
         
-    cur.close()        
+    cur.close() 
+    fh.close()       
     return 0
     
 
+def loadPlayerStats():
+    #open required databases and files
+    fields = ["PLAYER_ID","TEAM_ID","AGE","GAMES_PLAYED","WINS","LOSSES","MINS","PTS","FGM","FGA","FG_PERCENT","THREEPM","THREEPA","","FTM","FTA", "FT_PERCENT","OREB","DREB","REB","AST","TOV","STL","BLK","PF","FANTASY_POINTS","DD2","DD3","PLUS_MINUS"]
+    try: 
+        conn = sqlite3.connect("/Users/bryan/Desktop/CSCE/projects/CSCE-3444/Data-Functions/Players-Teams.db")
+        cur = conn.cursor()
+    except:
+        print("Cannot connect to database 'loadPlayerData()' failed") 
+        return -1 # -1 will be the error case
+    try:
+        fh = open("/Users/bryan/Desktop/CSCE/projects/CSCE-3444/Data-Functions/statsPage.html")
+    except:
+        print("Cannot open html file 'loadPlayerData()' failed")
+        return -1
+    
+    doc = BeautifulSoup(fh, "html.parser")
+    
+    playerTable = doc.find("tbody", attrs={"class": "Crom_body__UYOcU"})
+    # playesData = BeautifulSoup(str(playerTable), "html.parser")
+    players = playerTable.findAll("tr")
+    
+    def getATagData(lst, num):
+        try:
+            tempStream = BeautifulSoup(str(lst[num]), "html.parser")
+            item = tempStream.find("a").text
+            return item
+        
+        except:
+            with open("statsErrors.txt", "a") as fh:
+                fh.write(f"'{name}',")
+            return ""
+    
+    def getData(list, index):
+        try:
+            item = list[index].text
+            # print(item, name)
+            return item
+        except:
+            with open("statsErrors.txt", "a") as fh:
+                fh.write(f"'{item}',")
+            return ""
+            
+    
+    for playerIndex in range(len(players)):
+        playerDataStream = BeautifulSoup(str(players[playerIndex]), "html.parser")
+        playerData = playerDataStream.findAll("td")
+        
+        items = list()
+        
+        
+        #name_id
+        try: 
+            tempStream = BeautifulSoup(str(playerData[1]), "html.parser")
+            name = tempStream.find("a").text
+            name_id = cur.execute("Select id from Players where playerName =(?)", (name,)).fetchone()[0]
+            items.append(name_id)
+            # print(name_id, name)
+            
+        except:
+            with open("statsErrors.txt", "a") as fh:
+                fh.write(f"'{name}',")
+        try:
+        # team_id
+            tempStream = BeautifulSoup(str(playerData[2]), "html.parser")
+            team = tempStream.find("a").text
+            team_id = cur.execute("Select id from Teams where teamAbv =(?)", (team,)).fetchone()[0]
+            items.append(team_id)
+            # print(team_id, name)
+        except:
+            with open("statsErrors.txt", "a") as fh:
+                fh.write(f"'{name}',")
+                
+        for cellIndex in range(len(playerData)):
+            if (cellIndex < 3):
+                continue
+            item = getData(playerData, cellIndex)
+            if (len(item) == 0):
+                item = getATagData(playerData, cellIndex)
+            items.append(item)
+        print(name, end=" ")
+        
+        # items is the same length as the terms list should all line up
+        # cur.execute("INSERT OR IGNORE INTO StatsPerGame2022_2023 ('PLAYER_ID','TEAM_ID', 'AGE','GAMES_PLAYED','WINS','LOSSES','MINS','PTS','FGM','FGA','FG_PERCENT','THREEPM','THREEPA','FTM','FTA', 'FT_PERCENT','OREB','DREB','REB','AST','TOV','STL','BLK','PF','FANTASY_POINTS','DD2','DD3','PLUS_MINUS') values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", (items[0],items[1],items[2],items[3],items[4],items[5],items[6],items[7],items[8],items[9],items[10],items[11],items[12],items[13],items[14],items[15],items[16],items[17],items[18],items[19],items[20],items[21],items[22],items[23],items[24],items[25],items[26],items[27],items[28],items[29],))
+        # conn.commit()
+        print(items[0])
+        # print(len(fields), len(items))
+        
+        # for index in range(len(items)):
+        #     print(items[index], end=" ")
+        # print()
+                
+
+            
+        
+                
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        # try:
+        #     team = playerData.find("a", attrs={"class": "Anchor_anchor__cSc3P RosterRow_team__AunTP"}).text
+        # except: 
+        #     team = "NA"
+
+def loadExtra():
+    names = ['Juan Toscano-Anderson','KZ Okpala','Matthew Dellavedova','Noah Vonleh','Daishen Nix','John Wall','Justin Jackson','Vit Krejci','Frank Kaminsky','Hamidou Diallo','PJ Dozier','Serge Ibaka','Tyrese Martin','Bryn Forbes','R.J. Hampton','Ryan Arcidiacono','Leandro Bolmaro','Vernon Carey Jr.','Boban Marjanovic','Jarrett Culver','Kemba Walker','Stanley Johnson','Tony Bradley','Malcolm Hill','Rodney McGruder','Gorgui Dieng','Joshua Primo','Marko Simonovic','Trevor Keels','Facundo Campazzo','Sterling Brown','Trevelin Queen','Tyler Dorsey','Alize Johnson','Chima Moneke','Chris Silva','Deonte Burton','Devon Dotson','Donovan Williams','Jordan Hall','Kobi Simmons','Michael Foster Jr.','Skylar Mays','Chance Comanche','Frank Jackson','Jarrell Brantley','Jordan Schakel','Justin Minaya','RaiQuan Gray','Xavier Sneed']
+    try: 
+        conn = sqlite3.connect("/Users/bryan/Desktop/CSCE/projects/CSCE-3444/Data-Functions/Players-Teams.db")
+        cur = conn.cursor()
+    except:
+        print("Cannot connect to database 'loadPlayerData()' failed") 
+        return -1 # -1 will be the error case
+    try:
+        fh = open("/Users/bryan/Desktop/CSCE/projects/CSCE-3444/Data-Functions/test.html")
+    except:
+        print("Cannot open html file 'loadPlayerData()' failed")
+        return -1
+    
+    for name in names:
+        cur.execute("Insert or IGNORE into Players  (playerName,  currentTeam, jerseyNumber,  position, height, weight) values (?, ?, ?, ?, ?, ?)", (name, "NA", "NA", "NA", "NA", -1,))
+        conn.commit()
+    
+    cur.close()
+    return 0
+
+loadPlayerStats()
 
 
 
