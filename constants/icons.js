f939cdcd8739cee3f77c80305ee28e90c1ad1cf9
Bryan Samuels <bryansamuels123@gmail.com>
2023-10-19 14:32:01 -0500

    added new icons

--- constants/icons.js
+++ constants/icons.js
@@ -7,16 +7,18 @@ import collectionsBM from "../assets/icons/collections_bookmark_FILL0_wght400_GR
 import MainPageIcon from "../assets/icons/cottage_FILL0_wght400_GRAD0_opsz24.png"
 import remove from "../assets/icons/delete_FILL0_wght400_GRAD0_opsz24.png"
 import expand from "../assets/icons/expand_more_FILL0_wght400_GRAD0_opsz24.png"
-import teamsIcon from "../assets/icons/groups_FILL0_wght400_GRAD0_opsz24.png"
+import teamsIcon from "../assets/icons/teamIcon.png"
 import help from "../assets/icons/help_FILL0_wght400_GRAD0_opsz24.png"
 import accountSettings from "../assets/icons/manage_accounts_FILL0_wght400_GRAD0_opsz24.png"
 import menu from "../assets/icons/menu_FILL0_wght400_GRAD0_opsz24.png"
 import news from "../assets/icons/newspaper_FILL0_wght400_GRAD0_opsz24.png"
 import playersIcon from "../assets/icons/person_FILL0_wght400_GRAD0_opsz24.png"
-import playerCardIcon from "../assets/icons/person_book_FILL0_wght400_GRAD0_opsz24.png"
+import playerCardIcon from "../assets/icons/playerIcon.png"
 import playerSearch from "../assets/icons/person_search_FILL0_wght400_GRAD0_opsz24.png"
 import learnIcon from "../assets/icons/school_FILL0_wght400_GRAD0_opsz24.png"
 import settingsGear from "../assets/icons/settings_FILL0_wght400_GRAD0_opsz24.png"
+import learningTile from "../assets/icons/learningTile-U.png"
+
 
 export default {
     addCircle,
